de12ad059489a13727f20ba785c0cbda5f540895
UlrichIvan <mtchokos@gmail.com>
2026-02-23 15:45:40 +0100

    route for signup feature

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("guest")->group(function () {
+    // login
     Route::get("/", [AuthController::class, "index"])->name("login");
     Route::post("/", [AuthController::class, "login"]);
+    // signup
+    Route::get("/signup", [AuthController::class, "indexSignup"])->name("signup");
+    Route::post("/signup", [AuthController::class, "signup"]);
 });
