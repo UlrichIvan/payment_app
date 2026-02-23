66cd7840ebe8144769897708787ae9772bb01123
UlrichIvan <mtchokos@gmail.com>
2026-02-23 14:45:57 +0100

    user login done successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AuthController extends Controller
@@ -9,5 +11,15 @@ class AuthController extends Controller
     public function index()
     {
         return Inertia::render("Home");
+    }
+    public function login(LoginRequest $request)
+    {
+        $valided = $request->validated();
+
+        if (Auth::attempt($valided)) {
+            dd($valided);
+        } else {
+            return back()->with("message", "email ou mot de passe invalide");
+        }
     }
 }
