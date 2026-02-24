3d98483943da14d0bec6c64f791b1222036ada5b
UlrichIvan <mtchokos@gmail.com>
2026-02-24 13:19:48 +0100

    update ProductSeeder add reviews to products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
@@ -12,6 +13,10 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory(10)->create();
+        Product::factory(10)->create()->each(function ($product) {
+            Review::factory(5)->create([
+                'product_id' => $product->id,
+            ]);
+        });
     }
 }
