861661aeb163a0fe7422a95aba6e7f8d8b878678
UlrichIvan <mtchokos@gmail.com>
2026-02-25 09:05:55 +0100

    add image empty for empty cart

--- resources/js/Pages/Cart/Index.tsx
+++ resources/js/Pages/Cart/Index.tsx
@@ -3,16 +3,19 @@ import CartProduct from "@src/js/components/CartProduct";
 import Navbar from "@src/js/components/Navbar";
 import { PageProps } from "@src/js/types/global";
 import { Toaster } from "sonner";
+import empty from "@src/assets/images/png/empty-cart.png";
+import useFlash from "@src/js/hooks/useFlash";
 
 const Index = () => {
     const { cartItems } = usePage<PageProps>().props;
+    useFlash();
     return (
         <div className="container-fluid px-0 vh-100 overflow-y-auto overflow-x-hidden">
             <Navbar />
             {cartItems?.items && cartItems?.items.length > 0 ? (
                 <div className="row my-4">
-                    <div>
-                        <h3 className="text-center">Resumer de votre panier</h3>
+                    <h3 className="text-center">Resumer de votre panier</h3>
+                    <>
                         {cartItems?.items.map((product) => (
                             <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-2">
                                 <CartProduct
@@ -21,14 +24,15 @@ const Index = () => {
                                 />
                             </div>
                         ))}
-                    </div>
+                    </>
                 </div>
             ) : (
-                <div className="row my-4 justify-content-center align-align-items-center">
+                <div className="d-flex my-4 justify-content-center align-items-center">
                     <div>
                         <h3 className="text-center text-secondary">
                             Votre panier est vide
                         </h3>
+                        <img src={empty} alt="empty cart" />
                     </div>
                 </div>
             )}
