07526cc6c432e7f7ffc7a47ca4a20ae08b38b2cb
UlrichIvan <mtchokos@gmail.com>
2026-02-25 11:08:22 +0100

    add payment button on cart index page

--- resources/js/Pages/Cart/Index.tsx
+++ resources/js/Pages/Cart/Index.tsx
@@ -1,4 +1,4 @@
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import CartProduct from "@src/js/components/CartProduct";
 import Navbar from "@src/js/components/Navbar";
 import { PageProps } from "@src/js/types/global";
@@ -17,9 +17,18 @@ const Index = () => {
                     <h3 className="text-center text-decoration-underline">
                         Resumer de votre panier
                     </h3>
-                    <h3 className="text-center">
-                        Total à payé : {cartItems.total} €
-                    </h3>
+                    <div className="d-flex justify-content-center align-align-items-center my-1 p-2 z-2 gap-2">
+                        <h3 className="text-center">
+                            Total à payé : {cartItems.total} €
+                        </h3>
+                        <Link
+                            href={route("checkout")}
+                            method="post"
+                            className="btn btn-outline-success mx-2"
+                        >
+                            Effectuez le payement
+                        </Link>
+                    </div>
                     <>
                         {cartItems?.items.map((product) => (
                             <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-2">
